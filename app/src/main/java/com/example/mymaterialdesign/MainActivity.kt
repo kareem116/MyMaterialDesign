a338af9498bbe1614adbc6d568a5f12296de9aee
karim <karimalla8@gmail.com>
2023-03-02 03:34:33 +0200

    Fab

--- app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
+++ app/src/main/java/com/example/mymaterialdesign/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.mymaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mymaterialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // inisialize
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.extendedFab.setOnClickListener {
+            Toast.makeText(this, "you clicked me" ,Toast.LENGTH_LONG).show()
+        }
     }
 }
